f1dc67a83b85704a223de38ac21c82bfca5b9c8a
dom <dom@021ee608-1ed4-0310-a892-85bafff719e1>
2000-01-05 08:08:52 +0000

    Bring in initial version of the data structure library.

---
+++ src/spider/lib/ds/ds.h
@@ -0,0 +1,24 @@
+/***********************************************************************
+ * ds.h
+ *
+ * Copyright 2000 Dominic Mitchell (dom@myrddin.demon.co.uk)
+ *
+ * @(#) $Id: ds.h,v 1.1 2000/01/05 08:08:52 dom Exp $
+ */
+
+#ifndef _DS_H_
+#define _DS_H_ 1
+
+/* PROTOTYPES */
+
+int hash(char *);
+
+#endif /* _DS_H_ */
+
+/*
+ * Local variables:
+ * mode: C
+ * c-file-style: "BSD"
+ * comment-column: 32
+ * End:
+ */
