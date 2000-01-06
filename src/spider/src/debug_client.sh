36cd98daf48007dcd2c6036163c30f9b7533ecf7
dom <dom@021ee608-1ed4-0310-a892-85bafff719e1>
2000-01-06 07:23:54 +0000

    Allow awk to be configured in as well.

--- src/spider/src/debug_client.sh
+++ src/spider/src/debug_client.sh
@@ -2,12 +2,13 @@
 #
 # You might prefer to do this as an alias from your shell.
 #
-# $Id: debug_client.sh,v 1.1 1999/03/11 15:39:49 dom Exp $
+# $Id: debug_client.sh,v 1.2 2000/01/06 07:23:54 dom Exp $
 #
 
 config=!CONFIG!
+awk=!AWK!
 spider_host=${SPIDER_HOST:-localhost}
 
-port=`awk '$1=="Port" {print $2}' $config`
+port=`$awk '$1=="Port" {print $2}' $config`
 
 exec telnet $spider_host $port
--- src/spider/src/kill_spider.sh
+++ src/spider/src/kill_spider.sh
@@ -2,13 +2,14 @@
 #
 # A script to shutdown Spider, by sending it a SIGTERM.
 #
-# $Id: kill_spider.sh,v 1.1 1999/03/11 15:39:49 dom Exp $
+# $Id: kill_spider.sh,v 1.2 2000/01/06 07:23:54 dom Exp $
 #
 
 config=!CONFIG!
+awk=!AWK!
 
-spool_dir=`awk '$1=="Spool_dir" {print $2}' $config`
-pid_file=`awk '$1=="PID_File" {print $2}' $config`
+spool_dir=`$awk '$1=="Spool_dir" {print $2}' $config`
+pid_file=`$awk '$1=="PID_File" {print $2}' $config`
 
 cd $spool_dir
 kill `cat $pid_file`
