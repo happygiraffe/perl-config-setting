49b7259db23f88a0d9267aba1b21a6f0666182f2
dom <dom@021ee608-1ed4-0310-a892-85bafff719e1>
2000-01-12 21:36:08 +0000

    Add in initial Network Events library.  This is pretty much a dummy file at the moment.

---
+++ src/spider/lib/nev/nev.c
@@ -0,0 +1,48 @@
+/*********************************************************************
+ * nev.c
+ *
+ * Network EVents library.
+ *
+ * Copyright 2000 Dominic Mitchell (dom@myrddin.demon.co.uk)
+ *
+ * @(#) $Id: nev.c,v 1.1 2000/01/12 21:36:08 dom Exp $
+ */
+
+#include <sys/types.h>
+
+#include "nev.h"
+
+static const char rcsid[]="@(#) $Id: nev.c,v 1.1 2000/01/12 21:36:08 dom Exp $";
+
+int
+nev_listen(char *addr, char *port, nev_cbfn newconn_fn, void *data)
+{
+    return 0;
+}
+
+int
+nev_watch(int fd, nev_cbfn read_fn, nev_cbfn close_fn)
+{
+    return 0;
+}
+
+void
+nev_main_loop(void)
+{
+    return;
+}
+
+void
+nev_unwatch(int fd)
+{
+    close (fd);
+    return;
+}
+
+/*
+ * Local variables:
+ * mode: C
+ * c-file-style: "BSD"
+ * comment-column: 32
+ * End:
+ */
