5cb2009f866c77114aa94da5d403ee94de47c92e
dom <dom@021ee608-1ed4-0310-a892-85bafff719e1>
2000-07-04 10:44:30 +0000

    Implement a RipeObj class, which is easier to deal with rather than holding all the info in a list.

--- src/whoistools/ripe.py
+++ src/whoistools/ripe.py
@@ -4,17 +4,52 @@
 Perform a ripe query.
 """
 
-__rcs_id__='$Id: ripe.py,v 1.2 2000/07/04 10:11:55 dom Exp $'
-__version__='$Revision: 1.2 $'[11:-2]
+__rcs_id__='$Id: ripe.py,v 1.3 2000/07/04 10:44:30 dom Exp $'
+__version__='$Revision: 1.3 $'[11:-2]
 
 import sys
 import string
 import whois
 
+from types import ListType, TupleType
+
 #WHOISHOST = 'whois.ripe.net'
 WHOISHOST = 'localhost'
 
-def Ripe(query):
+class RipeObj:
+    """Representation of a RIPE object."""
+
+    def __init__(self, data):
+        """Create a new Ripe Object.
+
+        The data must be a list of tuples, each of length two.
+
+        """
+
+        # Check that what we have is formatted correctly.
+        assert type(data) == ListType
+        for rec in data:
+            assert len(rec) == 2
+
+        self.data = data
+        return
+
+    def __getattr__(self, val):
+        res = []
+        for o, a in self.data:
+            if o == val: res.append(val)
+        return res
+
+    def __str__(self):
+        res = ''
+        for o, a in self.data:
+            res = res + '%-13s %s\n' % (o+':', a)
+        return res[:-1]                 # Strip trailing NL.
+
+    def id(self):
+        return self.data[0][1]
+
+def RipeQuery(query):
     """Perform a ripe query and return the result as a list of tuples."""
 
     res = []
@@ -25,6 +60,7 @@ def Ripe(query):
     for line in data:
         if not line:
             if len(currec) > 0:
+                currec = RipeObj(currec)
                 res.append(currec)
             currec = []
             continue
@@ -37,10 +73,9 @@ def Ripe(query):
 
 if __name__ == '__main__':
     try:
-        from pprint import pprint
-        objs = Ripe(sys.argv[1])
-        #pprint(objs)
+        objs = RipeQuery(sys.argv[1])
         for o in objs:
-            print "%-13s %s" % (o[0][0] + ':', o[0][1])
+            print o
+            print
     except IndexError:
         print "usage: ripe query"
